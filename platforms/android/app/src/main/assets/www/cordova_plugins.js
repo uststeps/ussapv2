af743914fd005bc9beeaf1aa023ef616fdc1cef4
uststeps <uststeps@users.noreply.github.com>
2019-08-27 08:59:39 +0800

    x

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-app-event": "1.2.1"
   };
 });
