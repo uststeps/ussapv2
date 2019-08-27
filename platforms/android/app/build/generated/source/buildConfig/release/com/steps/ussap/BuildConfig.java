7fee64034c58aedb2fcdf0666c55d20f00e77501
uststeps <uststeps@users.noreply.github.com>
2019-08-27 08:37:38 +0800

    3.1.3

--- platforms/android/app/build/generated/source/buildConfig/release/com/steps/ussap/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/steps/ussap/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.steps.ussap";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 30102;
-  public static final String VERSION_NAME = "3.1.2";
+  public static final int VERSION_CODE = 30103;
+  public static final String VERSION_NAME = "3.1.3";
 }
